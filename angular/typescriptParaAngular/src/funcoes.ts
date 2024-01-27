ac9d245685da6fa6c2f9313838cac0a95bf56b9c
Alan Johnny <johnnypontes711@gmail.com>
2024-01-27 01:26:41 -0300

    sobre interfaces

---
+++ angular/typescriptParaAngular/src/funcoes.ts
@@ -0,0 +1,18 @@
+function addNumber(x: number, y: number): number{
+  return x + y
+}
+function addToHello(name: string){
+  return `Hello ${name}`;
+}
+  
+function CallToPhone(phone: number | string){
+  return phone
+}
+
+async function getDatabase(id: number): Promise<string|number> {
+  return 'facebook';
+}
+
+let soma: number = addNumber(7, 2);
+
+console.log();
