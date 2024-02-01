c8bdf6dfaab25d7c5329492f1943aa7e86af2674
Alan Johnny <johnnypontes711@gmail.com>
2024-01-31 23:59:03 -0300

    v2

--- angular/typescriptParaAngular/src/index.ts
+++ angular/typescriptParaAngular/src/index.ts
@@ -1,10 +1,38 @@
 //decorators
-function ExibirNome(target: any){
-  console.log(target);
+// function ExibirNome(target: any){
+//   console.log(target);
+// }
+
+// @ExibirNome
+// class Funcionario {}
+
+// @ExibirNome
+// class Quincas {}
+
+function apiVersion(version: string) {
+  return (target: any) => {
+    Object.assign(target.prototype, { __version: version, __name: "John"});
+  }
 }
 
-@ExibirNome
-class Funcionario {}
+// attribute decorator
 
-@ExibirNome
-class Quincas {}
+function  minLength(length:number) {
+  return  (target: any , key:string) => {
+    console.log(target[key]);
+    
+  }
+}
+
+class Api {
+    @minLength(3)
+    name: string;
+
+    constructor(name: string){
+      this.name = name;
+    }
+}
+
+const api = new Api("produtos");
+console.log(api.name);
+
